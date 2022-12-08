0b561e85976b848e09be49c802caeaf1cf8cf8b2
monikathorat <monikathorat0019@gmail.com>
2022-12-08 09:33:00 +0530

    JS variable file
    JS variable file

---
+++ 01_variable.js
@@ -0,0 +1,9 @@
+var myName;
+myName="Monika Thorat";
+console.log(myName);
+
+var myFrndName = "Bobo";
+console.log(myFrndName);
+
+myFrndName ="Gigi";
+console.log(myFrndName);
---
+++ 01_variable_assign.js
@@ -0,0 +1,21 @@
+var myFullName;
+console.log(myFullName);
+
+myFullName ="Monika Thorat";
+console.log(myFullName);
+
+myFullName ="Monika Haridas Thorat";
+console.log(myFullName);
+
+var myLove ="I love only JS";
+console.log(myLove);
+
+var firstName ="Monika";
+var lastName ="Thorat";
+var age ="26";
+var address ="A/P Sonke,Tal.Pandharpur,Dist.Solapur";
+var pin_code ="413 304";
+var gender ="Female";
+var is_married ="No";
+console.log(firstName,lastName,age,address,pin_code,gender,is_married);
+ 

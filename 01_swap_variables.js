328ff72010d63956e968233af6a87c90801f6f22
monikathorat <monikathorat0019@gmail.com>
2022-12-09 09:36:46 +0530

    var swapping file
    var swapping file

---
+++ 01_swap_variables.js
@@ -0,0 +1,9 @@
+var myNumber = 100;
+var yourNumber = 300;
+console.log("Before Swap");
+console.log(myNumber,yourNumber);
+var temp = myNumber;
+var myNumber =  yourNumber;
+var yourNumber = temp;
+console.log("After Swap");
+console.log(myNumber,yourNumber);

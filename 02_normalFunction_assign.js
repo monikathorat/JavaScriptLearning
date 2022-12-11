32fd24105f8c459ccd9533018d2c4ef6f3831666
monikathorat <monikathorat0019@gmail.com>
2022-12-11 14:34:47 +0530

    02function assign
    02function assign

---
+++ 02_normalFunction_assign.js
@@ -0,0 +1,6 @@
+function bankDetails(bankName, accountNum, location, pincode){
+    console.log("Here are the Bank Details :",  bankName, accountNum, location,  pincode);
+}
+bankDetails("Bank Name: CITI Bank,", "Account Number: 3456782345,", "Location: Pune,", "Pincode: 431202");
+bankDetails("Bank Name: Axis Bank,", "Account Number: 7856782345,", "Location: Mumbai,", "Pincode: 441202");
+bankDetails("Bank Name: HDFC Bank,", "Account Number: 8956782345,", "Location: Pune,", "Pincode: 631202");

2fa126f608a8f7a8f2ed99250993f9a2a5bc1e7b
monikathorat <monikathorat0019@gmail.com>
2022-12-21 11:55:47 +0530

    loops
    loops

---
+++ 05_forLoopAssign.js
@@ -0,0 +1,36 @@
+
+console.log(`###1. Print numbers from 5 to 15 by incrementing 1`);
+for (let index = 5; index <= 15; index++) {
+    console.log(index); 
+}
+console.log(``);
+console.log(`###2. Print numbers from 50 to 40 by decrementing 1 `);
+for (let index = 50; index >= 40; index--) {
+    console.log(index);   
+}
+console.log(``);
+console.log(`###3. First 15 odd numbers `);
+for (let index = 1; index <= 15; index= index+2) {
+    console.log(index);   
+}
+console.log(``);
+console.log(`###3. First 10 even numbers `);
+for (let index = 2; index <= 10; index= index+2) {
+    console.log(index);   
+}
+console.log(``);
+console.log(`###3. Table of 5 `);
+for (let index = 5; index <= 50; index=index+5) {
+    console.log(index);   
+}
+console.log(``);
+console.log(`###3. Table of 10 `);
+for (let index = 10; index <= 100; index=index+10) {
+    console.log(index)    
+}
+console.log(``);
+console.log(`###3. Table of 10 in reverse order `);
+
+for (let index = 100; index >= 10; index=index-10) {
+    console.log(index);   
+}

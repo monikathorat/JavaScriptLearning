4574525314a9d03005a5f6ad8cebf384ea351114
monikathorat <monikathorat0019@gmail.com>
2022-12-22 15:10:52 +0530

    count of characters
    count of characters

---
+++ 05_countCharAssign.js
@@ -0,0 +1,17 @@
+
+function countChar(string){
+    var counter = 0;
+    for(let index = 0; index < string.length; index++){
+        var char = string.charAt(index);
+        if (char =='a' || char == 'A') {
+            console.log(`The count of ${char}`);
+            counter = counter + 1;
+            
+        }
+    } 
+    console.log(`${string} ${counter}`);
+
+}
+countChar("I Am Learning JavaScript, The Language of internet");
+console.log(``);
+countChar("My favourite movieis LAggAn");
